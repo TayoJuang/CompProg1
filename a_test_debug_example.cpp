65aad1bf47902d6deb58fe55ec957c9ad7087d01
Tayo <tayojuang@gmail.com>
2025-07-01 14:12:42 +0000

    changed run setup

---
+++ a_test_debug_example.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+  // Uncomment the next line when debugging with file input
+  // freopen("a_test.input", "r", stdin);
+
+  ll x;
+  cin >> x;
+  ll y;
+  cout << x;
+  return 0;
+}
