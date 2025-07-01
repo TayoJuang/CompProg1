4daa4dc6e6c3dca1eb98760fbbec5126fd063ad0
Tayo <tayojuang@gmail.com>
2025-07-01 14:21:59 +0000

    gcpc ready

--- Week4_Strings/generate.py
+++
@@ -1,8 +0,0 @@
-with open("zero.input", 'w') as f:
-    #generate 500000 zero
-    line = "0 " * 500000 + "\n"
-    line2 = "1 " * 500000 + "\n"
-    f.write("500000 ")
-    f.write("500000\n")
-    f.write(line)
-    f.write(line2)
